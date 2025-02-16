975a3f7aa65d2e172e5365929c4a3f3485f5fb66
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-02-16 15:17:36 +0530

    v2

--- obj/Release/net8.0/E-Commerce.AssemblyInfo.cs
+++ obj/Release/net8.0/E-Commerce.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d5d7954531dba42e4371eb99879dc878c8b58e")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
